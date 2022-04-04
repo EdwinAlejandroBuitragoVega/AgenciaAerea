4870ba3f4607e4db0f98288dc7f66acf47ca4f82
USER <USER@DESKTOP-HNG3HQ1>
2022-04-04 14:41:32 -0500

    cambio

--- AgenciaAviones/src/agenciaaviones/Aviones.java
+++ AgenciaAviones/src/agenciaaviones/Aviones.java
@@ -13,10 +13,18 @@ public abstract class Aviones {
 
     private int velocidad;
     private String matricula;
+    private String hora;
+    private String Destino;
 
-    public Aviones(int velocidad, String matricula) {
+    public Aviones(int velocidad, String matricula,String hora, String Destino) {
         this.velocidad = velocidad;
         this.matricula = matricula;
+          this.hora = hora;
+        this.Destino = Destino;
+    }
+
+    public Aviones() {
+      
     }
 
     public int getVelocidad() {
@@ -29,6 +37,22 @@ public abstract class Aviones {
 
     public void setVelocidad(int velocidad) {
         this.velocidad = velocidad;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getDestino() {
+        return Destino;
+    }
+
+    public void setDestino(String Destino) {
+        this.Destino = Destino;
     }
 
     public void setMatricula(String matricula) {
